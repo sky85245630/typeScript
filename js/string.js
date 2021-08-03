e148a86e51eb5122e36e93b7aa4d511caaaf834d
Louie <louie.chen@baifu-tech.net>
2021-08-03 22:28:52 +0800

    string

---
+++ js/string.js
@@ -0,0 +1,8 @@
+"use strict";
+var str = '天氣';
+var str1 = '很好';
+var weather = '今天' + str + str1;
+var weather1 = "\u4ECA\u5929" + str + str1;
+//都輸出今天天氣很好
+console.log('weather', weather);
+console.log('weather1', weather1);
---
+++ ts/string.ts
@@ -0,0 +1,9 @@
+var str:string = '天氣';
+var str1:string = '很好';
+
+var weather:string = '今天'+str+str1
+var weather1:string = `今天${str}${str1}`
+
+//都輸出今天天氣很好
+console.log('weather',weather)
+console.log('weather1',weather1)
