85ecac6b2cbc5cfc0e4b939f16bdb59780509776
Louie <louie.chen@baifu-tech.net>
2021-08-03 22:17:22 +0800

    number

--- app.ts
+++ app.ts
@@ -1,3 +1,18 @@
-console.log('Hello ts');
-console.log('Hello ts');
-console.log('Hello ts');
+var num1:number = 100;
+var num2:number = 200.2;
+
+var num3 = 300;
+var num4 = 400.4;
+
+//寫這樣也可以
+//function add(a,b)
+function add(a:number,b:number){
+    let asd = a+b;
+    console.log('asd',asd)
+}
+
+//這樣就會報錯
+// add('100',200)
+
+//這樣不會報錯
+add(100,200)
--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 "use strict";
-console.log('Hello ts');
-console.log('Hello ts');
-console.log('Hello ts');
+var num1 = 100;
+var num2 = 200.2;
+var num3 = 300;
+var num4 = 400.4;
+//寫這樣也可以
+//function add(a,b)
+function add(a, b) {
+    var asd = a + b;
+    console.log('asd', asd);
+}
+//這樣就會報錯
+// add('100',200)
+//這樣不會報錯
+add(100, 200);
