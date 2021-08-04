21c19b0953e0a069fe0facc2603f671b2e5bf530
Louie <sky85245630@gmail.com>
2021-08-04 22:18:02 +0800

    touple

---
+++ js/touple.js
@@ -0,0 +1,8 @@
+"use strict";
+var userTouple;
+userTouple = [10, 'asd', 'zxc', true];
+var empTouple, emp = [
+    [10, 'asd'],
+    [11, 'zxc']
+];
+emp.push([12, 'XDDD']);
---
+++ ts/touple.ts
@@ -0,0 +1,10 @@
+var userTouple : [number,string,string,boolean]
+userTouple = [10,'asd','zxc',true]
+
+var empTouple:[number,string][],
+emp = [
+    [10,'asd'],
+    [11,'zxc']
+]
+
+emp.push([12,'XDDD'])
