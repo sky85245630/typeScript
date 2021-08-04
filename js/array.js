6be7691134b99c63041fc347009798f05f30a8a8
Louie <sky85245630@gmail.com>
2021-08-04 22:10:15 +0800

    array

---
+++ js/array.js
@@ -0,0 +1,5 @@
+"use strict";
+var list = [1, 2, 3];
+var strArr = ['1', '2', '3'];
+//or
+var listArr = [1, 2, 3];
---
+++ ts/array.ts
@@ -0,0 +1,5 @@
+var list:number[]=[1,2,3]
+var strArr:string[] = ['1','2','3']
+
+//or
+var listArr:Array<number> = [1,2,3]
