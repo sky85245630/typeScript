0f46678273c39395d977c229874f9f890e3d6087
Louie <sky85245630@gmail.com>
2021-08-04 22:31:24 +0800

    function

---
+++ js/function.js
@@ -0,0 +1,13 @@
+"use strict";
+function addFun(num1, number2) {
+    console.log(num1 + num2);
+}
+addFun(1, 2);
+//or
+function addFun1(num1, number2) {
+    return num1 + num2;
+}
+addFun1(1, 2);
+function showFun() {
+    return 'hello world';
+}
---
+++ ts/function.ts
@@ -0,0 +1,17 @@
+function addFun(num1:number,number2:number):void{
+    console.log(num1+num2)
+}
+
+addFun(1,2)
+
+//or
+
+function addFun1(num1:number,number2:number):number{
+    return num1+num2
+}
+
+addFun1(1,2)
+
+function showFun():string{
+    return 'hello world'
+}
